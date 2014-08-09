94ebf1a860b5054a5c7da9f385372b156cd72340
Ele Palmer <ecpalmer21@gmail.com>
2014-08-09 15:25:11 -0400

    Refactor
    Visitors are prompted to pick a number, and fizz buzz runs up to that
    number.

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,18 +1,30 @@
-var counter = 0;
-while (counter < 100) {
-    counter++;
-    if (counter % 3 === 0 && counter % 5 === 0) {
-        document.write("fizzbuzz" + "<br>")
+function fizzbuzz() {        
+        var counter = 1;
+        while (counter < theNumber) {
+        counter++;
+        if (counter % 3 === 0 && counter % 5 === 0) {
+            document.write("fizzbuzz" + "<br>")
+        }
+        else if (counter % 3 === 0) {
+            document.write("fizz" + "<br>");
+        }
+
+        else if (counter % 5 === 0) {
+            document.write("buzz" + "<br>");
+        }
+
+        else {
+            document.write(counter + "<br>");
+        }
+            }
+                }
+
+    var theNumber = Number(prompt("Pick a number between 1 and 1oo", " "));
+    if (!isNaN(theNumber)) {
+        fizzbuzz();
     }
-    else if (counter % 3 === 0) {
-        document.write("fizz" + "<br>");
-    }
-    
-    else if (counter % 5 === 0) {
-        document.write("buzz" + "<br>");
-    }
-    
     else {
-        document.write(counter + "<br>");
+        alert("That is not a number! Please refresh and try again."); 
     }
-}
+
+
